a19dbf52449ee7f64d004448848b20f6599acfc3
123Untitled <78928284+123Untitled@users.noreply.github.com>
2024-12-02 19:00:23 +0000

    add epoll.hpp

---
+++ inc/epoll.hpp
@@ -0,0 +1,38 @@
+#ifndef EPOLL_HPP
+# define EPOLL_HPP
+
+#include "ioevent.hpp"
+#include "unique_fd.hpp"
+
+#include <sys/epoll.h>
+#include <stdexcept>
+
+
+class epoll final {
+
+	private:
+
+		unique_fd _epoll;
+
+	public:
+
+		epoll();
+
+		epoll(const epoll&) = delete;
+		epoll(epoll&&) noexcept = default;
+		epoll& operator=(const epoll&) = delete;
+		epoll& operator=(epoll&&) noexcept = default;
+
+		~epoll() noexcept = default;
+
+
+		void add(ioevent&) const;
+
+
+		void del(const ioevent& io) const noexcept;
+
+
+		void wait() const;
+};
+
+#endif // EPOLL_HPP
