ce8824a291e2d0c1a0342854ce7af4f46b223ca6
123Untitled <78928284+123Untitled@users.noreply.github.com>
2024-12-06 18:42:08 +0000

    remove inotify

--- inc/tintin_reporter.hpp
+++ inc/tintin_reporter.hpp
@@ -1,197 +1,92 @@
 #ifndef TINTIN_REPORTER_HPP
 # define TINTIN_REPORTER_HPP
 
-#include <iostream>
-#include <fstream>
-#include <unistd.h>
-#include <cstring>
-#include "timestamp.hpp"
+# include <unistd.h>
+# include <sys/stat.h>
 
-#include "system/write.hpp"
+# include "timestamp.hpp"
+# include "file.hpp"
 
 
-class file_event {
-
-	public:
-
-		file_event(void) noexcept = default;
-		virtual ~file_event(void) noexcept = default;
-
-		virtual auto in_delete(void) -> void = 0;
-		virtual auto in_create(void) -> void = 0;
-		virtual auto in_modify(void) -> void = 0;
-		virtual auto in_ignored(void) -> void = 0;
-};
-
-#include <sys/stat.h>
-
-namespace sys {
-
-
-	template <unsigned N>
-	auto mkdir(const char (&path)[N], const mode_t& mode) -> void {
-		if (::mkdir(path, mode) == -1)
-			throw std::runtime_error("mkdir");
-	}
-
-	inline auto mkdir(const std::string_view& path, const mode_t& mode) -> void {
-		if (::mkdir(path.data(), mode) == -1)
-			throw std::runtime_error("mkdir");
-	}
-
-	template <unsigned N>
-	auto stat(const char (&path)[N]) -> struct ::stat {
-		struct ::stat st;
-
-		if (::stat(path, &st) == -1)
-			throw std::runtime_error("stat");
-
-		return st;
-	}
-
-	inline auto stat(const std::string_view& path) -> struct ::stat {
-		struct ::stat st;
-
-		if (::stat(path.data(), &st) == -1)
-			throw std::runtime_error("stat");
-
-		return st;
-	}
-
-}
-
-#include "file.hpp"
-
-
-class Tintin_reporter : public file_event {
-
-
-	private:
-
-		// -- private members -------------------------------------------------
-
-		/* log file */
-		file _log;
-
-		/* buffer */
-		std::string _buffer;
-
-
-		// -- private static members ------------------------------------------
-
-		/* log file path */
-		static constexpr const char* log_file = "/var/log/matt_daemon/matt_daemon.log";
-
-
-
-
-		static void _setup(void);
-
-
-		// -- private static methods ------------------------------------------
-
-		/* get shared instance */
-		static auto _shared(void) -> Tintin_reporter& {
-			static Tintin_reporter instance;
-			return instance;
-		}
-
-
-		// -- private lifecycle -----------------------------------------------
-
-		/* default constructor */
-		Tintin_reporter(void)
-		: _log{log_file, O_CREAT | O_WRONLY | O_APPEND, 0644},
-		  _buffer{} {
-
-			_setup();
-
-			// create folder and log file
-			// inotify::add("/var/log/matt_daemon", IN_IGNORED, IN_DELETE);
-		}
-
-
-		Tintin_reporter(const Tintin_reporter&) = delete;
-		Tintin_reporter &operator=(const Tintin_reporter&) = delete;
-
-		~Tintin_reporter(void) noexcept = default;
-
-
-	public:
-
-		void in_delete(void) override {
-
-			// recreate log file
-			_log = file{log_file, O_CREAT | O_WRONLY | O_APPEND, 0644};
-			// inotify::add("/var/log/matt_daemon", IN_IGNORED, IN_DELETE);
-		}
-
-		void in_create(void) override {
-		}
-
-		void in_modify(void) override {
-		}
-
-		void in_ignored(void) override {
-			in_delete();
-		}
-
+class Tintin_reporter final {
 
 
 	private:
 
 		template <size_t N>
-		void _report(const char (&type)[N], const char *message) {
+		static void _report(const char (&type)[N], const char *message) {
 
+			constexpr const char* folder[] {
+				"/var",
+				"/var/log",
+				"/var/log/matt_daemon",
+				nullptr
+			};
 
-			try {
+			for (size_t i = 0U; folder[i] != nullptr ; ++i) {
 
-				timestamp ts;
+				struct ::stat st;
 
-				_buffer.append(ts.view());
-				_buffer.append(type);
-				_buffer.append(message);
-				_buffer.append("\n");
+				if (::stat(folder[i], &st) == -1) {
 
-				sys::write(_log, _buffer);
+					if (errno != ENOENT)
+						return;
 
-				_buffer.clear();
+					if (::mkdir(folder[i], 0755) == -1)
+						return;
+				}
+
+				if (!S_ISDIR(st.st_mode))
+					return;
 			}
 
-			catch (const std::exception& e) {
-				// keep buffer
-			}
+
+			timestamp ts;
+
+			const int fd = ::open("/var/log/matt_daemon/matt_daemon.log",
+								O_WRONLY | O_APPEND | O_CREAT);
+
+			if (fd == -1)
+				return;
+
+			static_cast<void>(::write(fd, ts.time(), ts.size()));
+			static_cast<void>(::write(fd, type, N));
+			static_cast<void>(::write(fd, message, __builtin_strlen(message)));
+			static_cast<void>(::write(fd, "\n", 1U));
+
+			static_cast<void>(::close(fd));
 		}
+
+
+
+
+	public:
+
+		Tintin_reporter(void) = delete;
+		Tintin_reporter(const Tintin_reporter&) = delete;
+		Tintin_reporter(Tintin_reporter&&) = delete;
+		Tintin_reporter &operator=(const Tintin_reporter&) = delete;
+		Tintin_reporter &operator=(Tintin_reporter&&) = delete;
+		~Tintin_reporter(void) = delete;
 
 
 	public:
 
 		static void info(const char* message) {
-			_shared()._report("\x1b[32m    info\x1b[0m -> ", message);
+			_report("\x1b[32m    info\x1b[0m -> ", message);
 		}
 
 		static void error(const char* message) {
-			_shared()._report("\x1b[31m   error\x1b[0m -> ", message);
+			_report("\x1b[31m   error\x1b[0m -> ", message);
 		}
+
 		static void signal(const char* message) {
-			_shared()._report("\x1b[33m  signal\x1b[0m -> ", message);
+			_report("\x1b[33m  signal\x1b[0m -> ", message);
 		}
+
 		static void server(const char* message) {
-			_shared()._report("\x1b[34m  server\x1b[0m -> ", message);
+			_report("\x1b[34m  server\x1b[0m -> ", message);
 		}
-
-
-
-		//template <typename T>
-		//static void _print(std::ostream &os, const T& arg) {
-		//	os << arg << ' ';
-		//}
-		//
-		//
-		//template <typename... Ts>
-		//static void print(Ts&&... args) {
-		//
-		//}
 
 };
 
