134e7fba29401f6ce66ceb77d3df4cc8f88cd590
123Untitled <78928284+123Untitled@users.noreply.github.com>
2024-12-06 18:42:17 +0000

    remove inotify

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,8 @@
 #include "daemon.hpp"
 #include "tintin_reporter.hpp"
-#include "running.hpp"
-#include <vector>
 
-#include <sys/inotify.h>
-
-#include "unique_fd.hpp"
-
-#include "epoll.hpp"
 
 int	main(void) {
-
-
-	/*
-	std::cout << "alignof(struct ::inotify_event): " << alignof(struct ::inotify_event) << std::endl;
-	std::cout << "sizeof(struct ::inotify_event): " << sizeof(struct ::inotify_event) << std::endl;
-	return 0;
-	*/
-
-	/*
-	try {
-
-		const char* path = "yolo";
-
-		inotify in;
-
-		in.add(path, IN_CREATE | IN_DELETE | IN_MODIFY);
-
-		epoll::add(in);
-
-		while (true) {
-
-			epoll::poll();
-		}
-	}
-	catch (const std::exception &e) {
-		std::cerr << e.what() << std::endl;
-	}
-
-	return 0;
-	*/
-
-
-
-
-
 
 	try {
 
@@ -52,6 +10,7 @@ int	main(void) {
 		deamon.run();
 		return EXIT_SUCCESS;
 	}
+
 	catch (const std::exception &e) {
 
 		Tintin_reporter::error(e.what());
